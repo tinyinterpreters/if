b0827a41f303c32f8502a4bfe7af4be43fcfc8e6
Dwayne Crooks <dwayne@tinyinterpreters.dev>
2026-09-08 09:04:01 -0400

    Use a different flake.nix for the stepper

--- flake.nix
+++ flake.nix
@@ -2,18 +2,46 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-    ti.url = "github:tinyinterpreters/nix";
   };
 
-  outputs = { self, nixpkgs, flake-utils, ti }:
+  outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem(system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
-        mkElmShell = ti.lib.mkElmShell pkgs;
       in
       {
-        devShells.default = mkElmShell {
+        devShells.default = pkgs.mkShell {
           name = "if";
+
+          packages = [
+            pkgs.elmPackages.elm
+            pkgs.elmPackages.elm-format
+          ];
+
+          shellHook = ''
+            export PROJECT_ROOT="$(git rev-parse --show-toplevel)"
+            export PS1="($name)\n$PS1"
+
+            f () {
+              elm-format "$PROJECT_ROOT/src" "''${@:---yes}"
+            }
+
+            c () {
+              nix flake check -L &&
+              f --validate
+            }
+
+            clean () {
+              rm -rf "$PROJECT_ROOT/elm-stuff"
+            }
+
+            echo "Development environment loaded"
+            echo ""
+            echo "Type 'f' to run elm-format"
+            echo "Type 'c' to run all checks"
+            echo "Type 'clean' to remove build artifacts"
+            echo ""
+          '';
         };
       }
     );
